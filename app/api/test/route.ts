02128d1b89c192c20edecafda53196c032c77d82
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-07-04 03:10:02 +0900

    7. Error Handling

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -6,8 +6,10 @@ export async function GET(request: NextRequest) {
   const randomDelay = Math.floor(Math.random() * 5000);
   console.log("GET 요청 처리 중, 지연 시간:", randomDelay, "ms");
 
-  await delay(randomDelay);
+  throw new Error("테스트용 에러 발생!"); // 인위적으로 에러를 발생시킴
 
-  return NextResponse.json({ data: `${randomDelay}만에 응답완료!` });
+  //await delay(randomDelay);
+
+  //return NextResponse.json({ data: `${randomDelay}만에 응답완료!` });
 }
 
